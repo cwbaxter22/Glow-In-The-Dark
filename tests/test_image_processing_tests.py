f1748c9ab959ec0d188699562d58a4ce819cc776
Colin Baxter <cwbaxter@uw.edu>
2023-12-18 17:44:59 -0800

    Configured tests, setup continuous integration

--- tests/test_image_processing_tests.py
+++ tests/test_image_processing_tests.py
@@ -1,19 +1,26 @@
 import unittest
 from pressure_heatmapping import image_toolbox
 
+import numpy as np
+
 
 class Test_ImageProcessing(unittest.TestCase):
     '''DF creation: smoke (1), oneshot (2)
     
     Path to df defined in initialization,
     If folder is moved, most tests will fail.'''
-    def test_smoke_img_avg(self):
+    def test_one_shot_img_avg(self):
         """
         Pass a random set of arrays to image averaging function.
         If anything is returned, the test should pass. 
 
         """
+        matrix_one = np.ones([3, 3], int)
+        matrix_two = np.ones([3, 3], int)
+        list_matrix_ones = []
+        list_matrix_ones.append(matrix_one)
+        list_matrix_ones.append(matrix_two)
         try:
-            image_toolbox.img_avg(5)
+            self.assertAlmostEqual(image_toolbox.img_avg(list_matrix_ones).any(), 1)
         except RuntimeError:
             self.assertRaises(RuntimeError)
